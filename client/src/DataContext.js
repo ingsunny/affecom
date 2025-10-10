6616a12eb9bb4a8062f242b19f61571082687412
Sunny Patel <138172377+ingsunny@users.noreply.github.com>
2025-10-10 15:19:07 +0530

    Update DataContext.js

--- client/src/DataContext.js
+++ client/src/DataContext.js
@@ -31,7 +31,8 @@ export const DataProvider = ({ children }) => {
   const [brandLoading, setBrandLoading] = useState(false);
   const [brandError, setBrandError] = useState(null);
 
-  const API_BASE_URL = "http://localhost:5000/api";
+  // const API_BASE_URL = "http://localhost:5000/api";
+    const API_BASE_URL = "https://api.easyshopprice.com/api";
 
   // --- Initial Fetch: All Products and Categories ---
   useEffect(() => {
